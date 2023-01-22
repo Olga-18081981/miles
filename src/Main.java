ae051b59b98f7d860595786b2a99fb2b5b3480ac
Olga Soloveva <1Olga1@bk.ru>
2023-01-22 17:56:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int mile = 20; // одна миля равна двадцать рублей
+
+        int rubles = 100; // количество миль
+
+
+        System.out.println(rubles / mile);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
